cf3fbe259bb558924886512b7ccc2083b4bc6578
Marc Prud'hommeaux <marc@skip.tools>
2025-03-06 09:03:36 -0500

    Fix Linux package name

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let targets: [Target] = [
 #else
 let targets: [Target] = [
     .systemLibrary(name: "CZLib", pkgConfig: "zlib", providers: [.brew(["zlib"]), .apt(["zlib"])]),
-    .target(name: "ZIPFoundation", dependencies: ["CZLib"], cSettings: [.define("_GNU_SOURCE", to: "1")]),
-    .testTarget(name: "ZIPFoundationTests", dependencies: ["ZIPFoundation"])
+    .target(name: "ReadiumZIPFoundation", dependencies: ["CZLib"], cSettings: [.define("_GNU_SOURCE", to: "1")]),
+    .testTarget(name: "ReadiumZIPFoundationTests", dependencies: ["ZIPFoundation"])
 ]
 #endif
 
